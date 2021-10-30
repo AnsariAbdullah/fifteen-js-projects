080e74e45da2d51ba43e72d418ce43895c72f4c0
AnsariAbdullah <ansariabdullahar@gmail.com>
2021-10-30 17:26:45 +0530

    comment console

--- questions-projects/app.js
+++ questions-projects/app.js
@@ -4,7 +4,7 @@ const questions = document.querySelectorAll('.question');
 
 questions.forEach(function(question){
 	const btn = question.querySelector('.question-btn');
-	console.log(btn)
+	// console.log(btn)
 	btn.addEventListener('click', function(){
 
 		questions.forEach(function(item){
