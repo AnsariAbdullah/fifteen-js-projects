66780c6cb8dc060caf8ff322847cf166ae2fb7e1
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-01-17 14:43:56 +0530

    add loader functionality

--- video/app.js
+++ video/app.js
@@ -3,8 +3,10 @@
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 
 
+
 const btn = document.querySelector('.switch-btn');
 const video = document.querySelector('.video-container');
+const preloader = document.querySelector('.preloader');
 
 btn.addEventListener('click', function(){
 	if(!btn.classList.contains('slide')){
@@ -14,4 +16,9 @@ btn.addEventListener('click', function(){
 		btn.classList.remove('slide');
 		video.play();
 	}
+})
+
+// show loader before all items are loaded then hide loader
+window.addEventListener('load', function(){
+	preloader.classList.add('hide-preloader')
 })
